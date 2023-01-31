7d4305b24acba41ce40fd34ac08c4aa007db4632
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-01-31 14:18:28 +1100

    Add hover effect and move Learn JS

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-// const learnCards = document.querySelector(".cardHolder")
 
-// function createCard([img, title, description, link]) {
-//     let code = `
-//     <div class="card">
-//         <img src="${img}" class="card-img-top" alt="...">
-//         <div class="card-body">
-//             <h5 class="card-title">${title}</h5>
-//             <p class="card-text">${description}</p>
-//             <a href="${link}" class="btn btn-primary">Go somewhere</a>
-//         </div>
-//     </div>
-//     `;
-//     learnCards.innerHTML += code;
-// }
+// volunteer page, click botton event
+// document.addEventListener("DOMContentLoaded", function(){
+//     var submitBtn = document.getElementById("submit-btn");
+//     var contentDiv = document.getElementById("content");
+//     submitBtn.addEventListener("click", function(event){
+//         event.preventDefault();
+//         contentDiv.style.display = "block";
+//     });
+// });
 
-// let item1 = [example.png, "HTML Course", "This course teaches you how to make HTML from scratch", "https://www.codecademy.com/learn/learn-html"]
-// let item2 = [example.png, "CSS Course", "This course teaches you how to make CSS on your own", "https://www.codecademy.com/learn/learn-css"]
+document.addEventListener("DOMContentLoaded", function(){
+    var submitBtn = document.getElementById("submit-btn");
+    var contentDiv = document.getElementById("content");
+    var contentDiv2 = document.getElementById("content2");
+    submitBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        var selectedValue = document.querySelector('input[name="flexRadioDefault"]:checked').id;        
+        if(selectedValue == "project") {
+            contentDiv.style.display = "block";
+            contentDiv2.style.display = "none";
+        } else if(selectedValue == "tutor") {
+            contentDiv2.style.display = "block";
+            contentDiv.style.display = "none";
+        }
+    });
+});
 
-// createCard(item1)
-// createCard(item2)
-
-
-const learnCards = document.querySelector(".row")
+const learnCards = document.querySelector(".learnContainer")
 
 function createCard([img, title, description, link]) {
     let code = `
-        <div class="col">
-            <div class="card">
-                <img src="${img}" class="card-img-top" alt="...">
+    
+    <div class="col">        
+            <div class="card learnCard">
+                <img src="${img}" class="card-img-top1" alt="${title}">
                 <div class="card-body">
                     <h5 class="card-title">${title}</h5>
                     <p class="card-text">${description}</p>
                     <a href="${link}" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
-        </div>
-
+    </div>
+    
 
     `;
     learnCards.innerHTML += code;
 }
 
-let item1 = ["example.png", "HTML Course", "This course teaches you how to make HTML from scratch", "https://www.codecademy.com/learn/learn-html"]
-let item2 = ["example.png", "CSS Course", "This course teaches you how to make CSS on your own", "https://www.codecademy.com/learn/learn-css"]
+let HTML1 = ["example.png", "Codecademy HTML Course", "This course teaches you how to make HTML from scratch", "https://www.codecademy.com/learn/learn-html"]
+let CSS1 = ["example.png", "Codecademy CSS Course", "This course teaches you how to make CSS on your own", "https://www.codecademy.com/learn/learn-css"]
+let HTML2 = ["example.png", "FreeCodeCamp HTML Course", "This course teaches you how to make HTML from scratch", "https://www.freecodecamp.org/news/learn-html-beginners-course/"]
+let CSS2 = ["example.png", "FreeCodeCamp CSS Course", "This course teaches you how to make CSS on your own", "https://www.freecodecamp.org/news/learn-css-in-11-hours/"]
 
-createCard(item1)
-createCard(item2)
-createCard(item1)
-createCard(item2)
+createCard(HTML1)
+createCard(CSS1)
+createCard(HTML2)
+createCard(CSS2)
+
+
+
