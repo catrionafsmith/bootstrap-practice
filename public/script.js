2870272970b1ea2e8b52c14ff241e3d4e8dff526
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-04 12:00:46 +1100

    Separate out js scripts

--- public/script.js
+++ public/script.js
@@ -1,45 +1,6 @@
-$("#terminal").terminal(async function (command, terminal) {
-    try {
-        const prompt = `you are a helpful, knowledge sharing chatbot. I say: ${command}. You reply:`
-        const response = await fetch(
-            `https://api.openai.com/v1/completions`,
-            {
-                body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 256}),
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                    Authorization: "Bearer API_KEY",
-                },
-                    }
-        ).then((response) => {
-            if (response.ok) {
-                response.json().then((json) => {
-                    terminal.echo(json.choices[0].text.trim());
-                });
-            }
-        });
-      
-        console.log("Completed!");
-    } catch (err) { console.error(`Error: ${err}`) }
-},
-    {
-        greetings: 'GPT-3 Chatbot v0.1',
-        name: 'gpt3_demo',
-        height: 400,
-        width: 800,
-        prompt: '> '
-    });
 
 
 // volunteer page, click botton event
-// document.addEventListener("DOMContentLoaded", function(){
-//     var submitBtn = document.getElementById("submit-btn");
-//     var contentDiv = document.getElementById("content");
-//     submitBtn.addEventListener("click", function(event){
-//         event.preventDefault();
-//         contentDiv.style.display = "block";
-//     });
-// });
 
 document.addEventListener("DOMContentLoaded", function(){
     var submitBtn = document.getElementById("submit-btn");
