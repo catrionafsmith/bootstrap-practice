063481205d7d1f7e6016988c0667723b9bf5138c
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-04 11:47:12 +1100

    Add public folder

--- public/script.js
+++ public/script.js
@@ -29,6 +29,8 @@ $("#terminal").terminal(async function (command, terminal) {
         width: 800,
         prompt: '> '
     });
+
+
 // volunteer page, click botton event
 // document.addEventListener("DOMContentLoaded", function(){
 //     var submitBtn = document.getElementById("submit-btn");
@@ -58,12 +60,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
 const learnCards = document.querySelector(".learnContainer")
 
-function createCard([img, title, description, link]) {
+function createCard([type, title, description, link]) {
     let code = `
     
     <div class="col">        
             <div class="card learnCard">
-                <img src="${img}" class="card-img-top1" alt="${title}">
+            <div class="card-header">
+            ${type}
+          </div>
                 <div class="card-body">
                     <h5 class="card-title">${title}</h5>
                     <p class="card-text">${description}</p>
@@ -77,10 +81,10 @@ function createCard([img, title, description, link]) {
     learnCards.innerHTML += code;
 }
 
-let HTML1 = ["example.png", "Codecademy HTML Course", "This course teaches you how to make HTML from scratch", "https://www.codecademy.com/learn/learn-html"]
-let CSS1 = ["example.png", "Codecademy CSS Course", "This course teaches you how to make CSS on your own", "https://www.codecademy.com/learn/learn-css"]
-let HTML2 = ["example.png", "FreeCodeCamp HTML Course", "This course teaches you how to make HTML from scratch", "https://www.freecodecamp.org/news/learn-html-beginners-course/"]
-let CSS2 = ["example.png", "FreeCodeCamp CSS Course", "This course teaches you how to make CSS on your own", "https://www.freecodecamp.org/news/learn-css-in-11-hours/"]
+let HTML1 = ["HTML", "Codecademy HTML Course", "This course teaches you how to make HTML from scratch", "https://www.codecademy.com/learn/learn-html"]
+let CSS1 = ["CSS", "Codecademy CSS Course", "This course teaches you how to make CSS on your own", "https://www.codecademy.com/learn/learn-css"]
+let HTML2 = ["HTML", "FreeCodeCamp HTML Course", "This course teaches you how to make HTML from scratch", "https://www.freecodecamp.org/news/learn-html-beginners-course/"]
+let CSS2 = ["CSS", "FreeCodeCamp CSS Course", "This course teaches you how to make CSS on your own", "https://www.freecodecamp.org/news/learn-css-in-11-hours/"]
 
 createCard(HTML1)
 createCard(CSS1)
